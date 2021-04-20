2d5b0d1aa1c53bb3c6e9816562bfc65e26dce8cf
Tiaguin061 <tiaguin180@gmail.com>
2021-04-20 13:31:23 -0300

    perf: Removed router from the server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
 import './database';
+import routes from './routes';
 
 const app = express();
 
-app.post('/', (request, response) => {
-  
-});
+app.use(express.json());
+app.use(routes);
 
 const port = 3333;
 app.listen(port, () => console.log(`Server is running at port ${port}`));
